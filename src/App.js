057b8e2228ae86a24428c2677d0692eb5ee65b91
aayanshahirwar <aayanshahirwar@gmail.com>
2023-05-11 11:37:09 +0530

    Hello World

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Theek Hai.
+          Edit <code>src/App.js</code> and save to reload | Hello CDN World.
         </p>
         <a
           className="App-link"
