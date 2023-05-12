1a75dfe6e531aea2d6551e5fa8514036a1f2c1bc
aayanshahirwar <aayanshahirwar@gmail.com>
2023-05-12 17:20:38 +0530

    Hello World CDN

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Hello World This is REACT NODE CICD PIPELINE.
+          Edit <code>src/App.js</code> and save to reload Hello World CDN.
         </p>
         <a
           className="App-link"
